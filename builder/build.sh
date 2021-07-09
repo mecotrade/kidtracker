7c4387284bd97ad8905428157ba5e68c65821e9a
Sergey Shadchin <sergei.shadchin@gmail.com>
2021-07-10 02:05:48 +0300

    Execution privilege is granted to build.sh file, profile to frontend rebuild is added

--- builder/build.sh
+++ builder/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -f /build/src/main/resources/kidtracker.p12
+rm -rf /build/node_modules
 keytool -genkeypair -keystore /build/src/main/resources/kidtracker.p12 -storetype PKCS12 -storepass 12345678 -alias kidtracker -keyalg RSA -keysize 2048 -validity 99999 -dname "CN=Kidtracker SSL Certificate, OU=Kidtracker, O=Kidtracker, C=SA" -ext san=dns:${DOMAIN:-localhost},ip:${IP:-127.0.0.1}
 npm install
-mvn clean package -Dmaven.test.skip=true
+mvn clean package -P with-frontend -Dmaven.test.skip=true
